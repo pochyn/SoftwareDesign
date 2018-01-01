0cbd4b47238710a3ac9fbb65c0c774a2c9ffbf30
pochyn <staspochyn@gmail.com>
2018-01-01 21:09:03 +0100

    Add files via upload

---
+++ PhotoOranizer/DirectoryReader.java
@@ -0,0 +1,40 @@
+package directory_reader;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
+/**
+ * Select, read, and print the contents of a directory.
+ */
+public class DirectoryReader {
+
+	/**
+	 * Select a directory, then print the full path to the directory and its
+	 * contents, one per line. Prefix the contents with a hyphen and a space.
+	 *
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int returnVal = fileChooser.showOpenDialog(null);
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+			System.out.println(file);
+			
+		File [] a = file.listFiles();
+		for(File dir:a){
+			if(dir.exists() && dir.isDirectory()){
+				System.out.println("- "+dir.getName()+"/");
+			} else{
+				System.out.println("- "+dir.getName());
+			}
+			
+		}
+		
+		}
+	}
+}
